116e991f648b8d76075e917e74866de25d25687f
Bennett Rogers <bennettrogers@gmail.com>
2017-06-07 23:27:17 -0700

    generating package.json on browser build

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,29 @@
 var path = require('path')
 var webpack = require("webpack")
+var GenerateJsonPlugin = require('generate-json-webpack-plugin')
+var packageJson = require('./package.json')
 
 var entry = './src/bundle.ts'
 var entryWithWorker = './src/bundle.worker.ts'
+
+var packagePlatformJson = {
+    'name': 'node-vibrant',
+    'version': packageJson.version,
+    'keywords': packageJson.keywords,
+    'homepage': packageJson.homepage,
+    'license': packageJson.license,
+    'files': [
+      'vibrant.js',
+      'vibrant.js.map',
+      'vibrant.min.js',
+      'vibrant.worker.js',
+      'vibrant.worker.js.map',
+      'vibrant.worker.min.js',
+    ],
+    'repository': packageJson.repository,
+    'dependencies': packageJson.dependencies,
+};
+
 module.exports = {
     entry: {
         'vibrant': entry,
@@ -33,6 +54,12 @@ module.exports = {
         new webpack.optimize.UglifyJsPlugin({
             include: /\.min\.js$/,
             minimize: true
-        })
+        }),
+        new GenerateJsonPlugin(
+            'package.json',
+            packagePlatformJson,
+            undefined,
+            2
+        )
     ]
 }
